c74a67da46e9082076216839f1d34ae22472a451
Liam Watts <elnewatts@gmail.com>
2023-04-07 21:53:37 +0100

    Added html styling, and fmt main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-use std::{fs, sync::{Arc, atomic::{AtomicUsize, Ordering}}, collections::HashMap, borrow::Cow, process, path::Path};
-use serde_json::{Result, Value, json};
-use futures_util::{StreamExt, SinkExt, TryFutureExt};
+use futures_util::{SinkExt, StreamExt, TryFutureExt};
+use serde_json::{json, Value};
+use std::{
+    borrow::Cow,
+    collections::HashMap,
+    fs,
+    path::Path,
+    process,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+};
 use tokio_stream::wrappers::UnboundedReceiverStream;
-use warp::{ws::{Message, WebSocket}, Filter};
+use warp::{
+    ws::{Message, WebSocket},
+    Filter,
+};
 
 use quick_replace::worker;
 
-use tokio::sync::{mpsc::{self, UnboundedSender}, RwLock};
-
+use tokio::sync::{
+    mpsc::{self},
+    RwLock,
+};
 
 static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 
@@ -15,26 +30,23 @@ type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Message>>>>;
 
 #[tokio::main]
 async fn main() {
-
     let users = Users::default();
     let users = warp::any().map(move || users.clone());
 
     let websocket_upgrade = warp::path("ws")
         .and(warp::ws())
         .and(users)
-        .map(|ws: warp::ws::Ws, users| {
-            ws.on_upgrade(move |socket| user_connected(socket, users))
-        });
-    
+        .map(|ws: warp::ws::Ws, users| ws.on_upgrade(move |socket| user_connected(socket, users)));
+
     // TODO: 404.
-    let index = warp::path::end().map(|| warp::reply::html(fs::read_to_string("./static/index.html").unwrap()));
+    let index = warp::path::end()
+        .map(|| warp::reply::html(fs::read_to_string("./static/index.html").unwrap()));
 
     let files = warp::path("temp").and(warp::fs::dir("./temp/"));
 
     let routes = index.or(websocket_upgrade).or(files);
 
-    warp::serve(routes).run(([127,0,0,1], 3000)).await;
-
+    warp::serve(routes).run(([127, 0, 0, 1], 3000)).await;
 }
 
 async fn user_connected(ws: WebSocket, users: Users) {
@@ -46,9 +58,7 @@ async fn user_connected(ws: WebSocket, users: Users) {
     let mut rx = UnboundedReceiverStream::new(rx);
 
     tokio::task::spawn(async move {
-        
         while let Some(message) = rx.next().await {
-            
             let parsed_json: Value = serde_json::from_str(message.to_str().unwrap()).unwrap();
 
             let msg: Message = if parsed_json["isOnlyMatches"] == true {
@@ -58,7 +68,7 @@ async fn user_connected(ws: WebSocket, users: Users) {
                 let from = parsed_json["data"]["from"].as_str().unwrap();
                 let to = parsed_json["data"]["to"].as_str().unwrap();
                 let filename = parsed_json["data"]["filename"].as_str().unwrap();
-         
+
                 let result = data.replace(&from, &to);
 
                 let filepath = format!("./temp/{}", filename);
@@ -73,9 +83,10 @@ async fn user_connected(ws: WebSocket, users: Users) {
                 Message::text(json.to_string())
             };
 
-            user_tx.send(msg).unwrap_or_else(|e| {
-                eprintln!("Websocket err: {}", e)
-            }).await;
+            user_tx
+                .send(msg)
+                .unwrap_or_else(|e| eprintln!("Websocket err: {}", e))
+                .await;
         }
     });
 
@@ -92,12 +103,9 @@ async fn user_connected(ws: WebSocket, users: Users) {
     }
 
     user_disconnected(user_id, &users).await;
-
-
 }
 
-
-async fn user_request(user_id: usize, msg: Message, users: &Users) {    
+async fn user_request(user_id: usize, msg: Message, users: &Users) {
     let msg = if let Ok(s) = msg.to_str() {
         s
     } else {
@@ -107,11 +115,11 @@ async fn user_request(user_id: usize, msg: Message, users: &Users) {
     let parsed_user_req: Value = serde_json::from_str(&msg).unwrap_or_else(|e| {
         println!("Error parsing json: {}", e);
         process::exit(0)
-    });  
+    });
 
     let from = &parsed_user_req["from"].as_str().unwrap().to_owned();
     let data = &parsed_user_req["data"].as_str().unwrap().to_owned();
-    
+
     let str_matches = data.matches(from).into_iter().count();
 
     let match_json = json!({
@@ -124,7 +132,7 @@ async fn user_request(user_id: usize, msg: Message, users: &Users) {
         "matches": str_matches,
         "data": parsed_user_req,
     });
-    
+
     for (&uid, tx) in users.read().await.iter() {
         if user_id == uid {
             if let Err(_error) = tx.send(Message::text(&user_req_json.to_string())) {};
